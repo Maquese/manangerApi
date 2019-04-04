89321bd3fb65691ab439dd4ebd295b71548e5d60
Your Name <kenneymaquese@gmail.com>
2019-04-04 11:28:57 -0300

    iversao de controle

---
+++ ManangerAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManangerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        
+    }
+}
--- ManangerAPI/Startup.cs
+++ ManangerAPI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ManangerAPI.Application.Contratos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +26,10 @@ namespace ManangerApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSingleton<IApplication,ManangerAPI.Application.ApplicationApp.Application>();            
+            services.AddSingleton<IUsuarioApplication,ManangerAPI.Application.ApplicationApp.Application>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
