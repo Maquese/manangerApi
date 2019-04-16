140297be53ebaddb75d89e2deb42440138b7b461
Your Name <kenneymaquese@gmail.com>
2019-04-16 14:51:46 -0300

    gitignore fix

--- ManangerAPI.Data/Entidades/Usuario.cs
+++ ManangerAPI.Data/Entidades/Usuario.cs
@@ -8,5 +8,6 @@ namespace ManangerAPI.Data.Entidades
         public string Nome { get; set; }
         public string Login { get; set; }
         public string Senha { get; set; }
+
     }
 }
--- ManangerAPI.Data/Migrations/20190415175855_ManangerMG2.Designer.cs
+++ ManangerAPI.Data/Migrations/20190415175855_ManangerMG2.Designer.cs
@@ -3,14 +3,16 @@ using ManangerAPI.Data.Contexto;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ManangerApi.Data.Migrations
 {
     [DbContext(typeof(ContextoDb))]
-    partial class ContextoDbModelSnapshot : ModelSnapshot
+    [Migration("20190415175855_ManangerMG2")]
+    partial class ManangerMG2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
