bd44a50c4b53e614e47c1a2f16a3d98ffa8425bf
giuliano_clastech <giuliano@clastech.com.br>
2019-09-12 14:52:50 -0300

    save tarefas

--- ManangerAPI.Application/ApplicationApp/TarefaApplication.cs
+++ ManangerAPI.Application/ApplicationApp/TarefaApplication.cs
@@ -12,19 +12,30 @@ namespace ManangerAPI.Application.ApplicationApp
     {
         public void AdicionarTarefa(string titulo, int contratoId, DateTime dataInicio, DateTime dataFim, TimeSpan horaInicio, TimeSpan horaFim)
         {
-            _tarefaRepositorio.Insert(new Tarefa{Titulo = titulo, ContratoId = contratoId, DataInicio = dataInicio, DataFim = dataFim, 
-                                                 HoraInicio = horaInicio, HoraFim = horaFim, Status = (int)StatusEnum.Ativo});
+            _tarefaRepositorio.Insert(new Tarefa
+            {
+                Titulo = titulo,
+                ContratoId = contratoId,
+                DataInicio = dataInicio,
+                DataFim = dataFim,
+                HoraInicio = horaInicio,
+                HoraFim = horaFim,
+                Status = (int)StatusEnum.Ativo
+            });
+            _tarefaRepositorio.Save();
+
         }
 
         public IList<TarefaDTO> ListarTarefasPorContrato(int contratoId)
         {
-            return _tarefaRepositorio.ListarTarefasPorContrato(contratoId).Select(x => new TarefaDTO{
+            return _tarefaRepositorio.ListarTarefasPorContrato(contratoId).Select(x => new TarefaDTO
+            {
                 ContratoId = x.ContratoId,
                 Titulo = x.Titulo,
                 DataInicio = x.DataInicio,
-                DataFim = x.DataFim, 
+                DataFim = x.DataFim,
                 HoraInicio = x.HoraInicio,
-                HoraFim = x.HoraFim,                
+                HoraFim = x.HoraFim,
             }).ToList();
         }
     }
